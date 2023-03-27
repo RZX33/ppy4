c020c10494939e69816f69fdf31e7e9b704d770f
RZX33 <s24794@pjwstk.edu.pl>
2023-03-27 17:15:30 +0200

    ver 1.0.

--- task3.py
+++ task3.py
@@ -1,33 +1,33 @@
-def caesars_cipher(data, k):
-    text = str(data).lower()
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z']
-
-    result = ''
-    for i in text:
-        if alphabet.__contains__(i):
-            temp_value = 0
-            if ord(i) + k > ord('z'):
-                temp_value = ord(i) + k - ord('z') + ord('a')
-            elif ord(i) + k < ord('a'):
-                temp_value = ord(i) + k + ord('z') - ord('a')
-            else:
-                temp_value = ord(i) + k
-            result += chr(temp_value)
-        else:
-            result += i
-    return result
-
-
 def caesars_cipher(data, k, arr=['']):
-    text = str(data)
-    result = ''
-    for i in text:
-        if i.lower() == arr[0]:
-            result += arr[1]
-        else:
-            result += i
-    return result
+    if arr.__contains__(''):
+        text = str(data).lower()
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                    'v', 'w', 'x', 'y', 'z']
+
+        result = ''
+        for i in text:
+            if alphabet.__contains__(i):
+                temp_value = 0
+                if ord(i) + k > ord('z'):
+                    temp_value = ord(i) + k - ord('z') + ord('a')
+                elif ord(i) + k < ord('a'):
+                    temp_value = ord(i) + k + ord('z') - ord('a')
+                else:
+                    temp_value = ord(i) + k
+                result += chr(temp_value)
+            else:
+                result += i
+        return result
+    else:
+        text = str(data)
+        result = ''
+        for i in text:
+            if i.lower() == arr[0]:
+                result += arr[1]
+            else:
+                result += i
+        return result
+
 
 
 print(caesars_cipher('The Project Gutenberg eBook of Alice’s Adventures in Wonderland, by Lewis Carroll', 5))
